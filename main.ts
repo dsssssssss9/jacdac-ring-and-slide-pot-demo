0ea25f3c297a3dd8077c7437380fb43241fe8b98
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-10 20:04:44 +0100

    add slide pot use plot graph on MB to test

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     modules.led1.setAll(0xff0000)
 })
-input.onButtonPressed(Button.AB, function () {
-    modules.led1.setAll(0x000000)
-})
 input.onButtonPressed(Button.B, function () {
     modules.led1.setAll(0x0000ff)
 })
+input.onButtonPressed(Button.AB, function () {
+    modules.led1.setAll(0x000000)
+})
 modules.led1.setBrightness(9)
 basic.forever(function () {
-	
+    led.plotBarGraph(
+    modules.potentiometer1.position(),
+    100
+    )
 })
