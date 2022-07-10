d9896fd7c53893f3eb0c8f95618684d42df30d8b
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-10 19:32:02 +0100

    ring test set bright & change colours

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    modules.led1.setAll(0xff0000)
+})
+input.onButtonPressed(Button.AB, function () {
+    modules.led1.setAll(0x000000)
+})
+input.onButtonPressed(Button.B, function () {
+    modules.led1.setAll(0x0000ff)
+})
+modules.led1.setBrightness(9)
 basic.forever(function () {
 	
 })
